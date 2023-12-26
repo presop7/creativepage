3836720c2312a7bbf16b9c57d71e63f95a519880
Presiyan Peev <130151109+Rez47@users.noreply.github.com>
2023-12-27 01:31:22 +0200

    Update Layout.jsx
    editing import paths in repository because of push bug

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,5 +1,5 @@
-import Nav from "../components/Nav.jsx";
-import Footer from "../components/Footer.jsx";
+import Nav from "../components/nav.jsx";
+import Footer from "../components/footer.jsx";
 
 export default function Layout({ children }) {
   return (
